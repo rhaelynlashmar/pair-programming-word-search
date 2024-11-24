1fcab90bd4d66951e92772d01573c01f53850a24
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-24 15:02:14 -0800

    feat-add transpose function for vertical wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
+// Transpose Function
+
+const transpose = function (matrix) {
+    // declare rows & declare columns as empty arrays
+    // create transpose array thats currently empty as well
+    // then we reverse the values and push into new array
+    const rows = matrix.length;
+    const columns = matrix[0].length;
+    let transposed = [];
+  
+    for (let i = 0; i < columns; i++) {
+      transposed[i] = [];
+    }
+    // console.log(transposed);
+    for (let column = 0; column < columns; column++) {
+      for (let row = 0; row < rows; row++) {
+        transposed[column][row] = matrix[row][column];
+      }
+    }
+    return transposed;
+  };
+
+// WordSearch function 
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    if (!word || letters.length === 0) {
+      return false;
+    }
+    const transposed = transpose(letters);
+    let fullArray = letters.concat(transposed);
+    const horizontalJoin = fullArray.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
+    return false;
+};
 
+  
 module.exports = wordSearch
