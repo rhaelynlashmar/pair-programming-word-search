eb717c0b76409c20c81a498803ebd79fcb02c747
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-24 15:14:36 -0800

    feat- add tests for vertical words, no words, empty letter array

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,45 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+
+  it("should return true if the word is vertically present", function() {
+  const result = wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['L', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['U', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['M', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'ASYLUM');
+
+  assert.isTrue(result);
+  });
+
+  it("should return false if there is no word", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], );
+
+    assert.isFalse(result);
+  });
+
+    it("should return false if passed an empty array", function() {
+      const result = wordSearch([], 'HELLO');
+  
+      assert.isFalse(result);
+    });
+
+
 });
